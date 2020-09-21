aacbefe8f63d10498ec8170fe363c4724972e875
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-09-21 16:36:16 +0700

    config taobang

---
+++ src/main/java/com/codegym/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,6 @@
+package com.codegym.service.impl;
+
+import com.codegym.service.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService {
+}
---
+++ src/main/java/com/codegym/service/impl/ProvinceServiceImpl.java
@@ -0,0 +1,6 @@
+package com.codegym.service.impl;
+
+import com.codegym.service.ProvinceService;
+
+public class ProvinceServiceImpl implements ProvinceService {
+}
